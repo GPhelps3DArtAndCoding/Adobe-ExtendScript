e639765aea6670f1a1970e54572456db8e1a7cc2
Gary <gpmail1@gmail.com>
2024-08-02 16:03:03 -0500

    ExtendScript Snipets Update

--- ExtendScript_Snipets/ExtendScript_BasicSnipets.jsx
+++ ExtendScript_Snipets/ExtendScript_BasicSnipets.jsx
@@ -1,2 +1,47 @@
-//Alert
-alert("hello there");
+//Gary Phelps 2024 gpmail1@gmail.com
+
+/*
+Nomenclature
+    * Il: Illustrator
+    * In: InDesign
+    * Ps: Photoshop
+*/
+//Basic Commands--------------------
+    
+    //Alert (In)
+        alert("hello there");
+
+//Document--------------------------
+ 
+    //Access Active Document (In)
+        var document = app.activeDocument;
+
+    //Name Document (In)
+        varName.name = "name";
+        //example
+        newDocument.name = "Summer Ad";
+ 
+    //New Document (In)
+        var newDocument = app.documents.add();
+
+//Pages-------------------------------
+    
+    //Access Pages (In)
+        var pages = document.pages;
+
+    //Number of Pages (In)
+        alert(pages.length);
+
+//Layers------------------------------
+    //INFO: layers is an array, starting at top layer at index 0
+    
+    //Access Layer Stack (In)
+        var layers = document.layers;
+    
+    //Show/Get Name of a Layer at index i (In)
+        alert(layer[i].name);
+    
+    //Number of Layers (In)
+        alert(layer.length);
+
+//Swatches-----------------------------
