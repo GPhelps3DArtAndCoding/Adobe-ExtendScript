43c5d963d0550153fcd890a73ee3c0a5ecbd6a3d
Gary <gpmail1@gmail.com>
2024-08-02 16:48:50 -0500

    ExtendScript Snipets Update

--- ExtendScript_Snipets/ExtendScript_BasicSnipets.jsx
+++ ExtendScript_Snipets/ExtendScript_BasicSnipets.jsx
@@ -1,10 +1,18 @@
 //Gary Phelps 2024 gpmail1@gmail.com
 
 /*
+Input Sources for This File
+
+* Adobe Indesign Scripting Tutorial: Create a Basic Sccript
+Youtube, NT Productions, https://www.youtube.com/watch?v=r1WWK7pl6so
+
+*/
+
+/*
 Nomenclature
-    * Il: Illustrator
-    * In: InDesign
-    * Ps: Photoshop
+    * (Il): Illustrator
+    * (In): InDesign
+    * (Ps): Photoshop
     * *text* means text is a variable
 */
 //Basic Commands--------------------
@@ -22,6 +30,15 @@ Nomenclature
  
     //New Document (In)
         var newDocument = app.documents.add();
+
+//Input
+    //File input
+        var inputFile = File("File name or path to file here");
+        var inputData;
+        *inputFile*.open("r");
+        *inputData* = *inputFile*.read().toString();
+        *inputFile*.close();
+        //see https://www.youtube.com/watch?v=r1WWK7pl6so @ 15:47
 
 //Layers------------------------------
     //INFO: layers is an array, starting at top layer at index 0
@@ -66,3 +83,18 @@ Nomenclature
 
     //Create a Text Frame (In)
         var newTextFrame = *newPage*.textFrames.add();
+
+    //Location and Size of Text Frame
+        *newTextFrame*.geometricBounds = [y1,x1,y2,x2]
+        /* y1,x1 = y,x coordinates of the top left corner
+           y2,x2 = y,x coordinates of the bottom right corner */
+
+    //QR Code (In)
+        *newTextFrame*.createPlainQRCode("url for QR Code here");
+
+    //Text (Content) of Text Frame (put text into it) (In)
+        *newTextFrame*.contents = "Your chosen text here";
+        /* Note: The text frame might be really small,
+        typically places in the upper, left corner. See "Location
+        and Size of Text Frame" above to create text frames of a 
+        specific size. */
