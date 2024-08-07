a4ddd974f531673dc4976c52c556a9f313292542
[Gary Phelps] <[gpmail1@gmail.com]>
2024-08-07 15:49:56 -0500

    moved all variables to top

--- includes/letter_halfInchMargins.jsx
+++ includes/letter_halfInchMargins.jsx
@@ -1,16 +1,18 @@
-//Global Variables
+//Variables
 var size = doc.documentPreferences;
 var marginPrefs = doc.pages[0].marginPreferences;
-
-//Set the page size
-size.pageWidth = "8.5 in";
-size.pageHeight = "11in";
 
 var marginDefault = ".5 in";
 var topMargin = marginDefault;
 var leftMargin = marginDefault;
 var rightMargin = marginDefault;
 var bottomMargin = marginDefault;
+
+//-------------------------------------------------
+
+//Set the page size
+size.pageWidth = "8.5 in";
+size.pageHeight = "11in";
 
 //Set the margins
 marginPrefs.properties = {
