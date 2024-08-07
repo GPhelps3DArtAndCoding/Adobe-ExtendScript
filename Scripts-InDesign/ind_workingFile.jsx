bc5b57e76a5e042feba141cfc752731eb7d01b8c
[Gary Phelps] <[gpmail1@gmail.com]>
2024-08-07 16:31:41 -0500

    add in inDesign working file

---
+++ Scripts-InDesign/ind_workingFile.jsx
@@ -0,0 +1,7 @@
+/* This file is a work in progress. It is where I'll add the most useful scripts
+and work on creating a UI Script Launcher or sorts. */
+
+//Create a new document
+#include "../includes/letter_halfInchMargins.jsx";
+
+var doc = createLetterDoc();
