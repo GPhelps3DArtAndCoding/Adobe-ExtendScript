f6745e99faaa34825c2730de1cb6af3aec539b6a
[Gary Phelps] <[gpmail1@gmail.com]>
2024-08-07 16:13:29 -0500

    improved code

--- Scripts-InDesign/indTest.jsx
+++ Scripts-InDesign/indTest.jsx
@@ -1,3 +1,4 @@
 //Create a new document
-var doc = app.activeDocument; //
 #include "../includes/letter_halfInchMargins.jsx";
+
+var doc = createLetterDoc();
--- includes/letter_halfInchMargins.jsx
+++ includes/letter_halfInchMargins.jsx
@@ -1,21 +1,26 @@
-//Variables
-var size = doc.documentPreferences;
-var marginPrefs = doc.pages[0].marginPreferences;
+//Create a letter sized document with 1/2 inch margins
+function createLetterDoc(){
 
-var marginDefault = ".5 in";
-var topMargin = marginDefault;
-var leftMargin = marginDefault;
-var rightMargin = marginDefault;
-var bottomMargin = marginDefault;
+  //Create a new document
+  var doc = app.documents.add();
 
-//-------------------------------------------------
+  //Variables
+  var size = doc.documentPreferences;
+  var margins= doc.pages[0].marginPreferences;
+  var halfInch = ".5 in";
+  
+  //--------------------------------------------------
 
-//Set the page size
-size.pageWidth = "8.5 in";
-size.pageHeight = "11in";
+  //Set the page size
+  size.pageWidth = "8.5 in";
+  size.pageHeight = "11 in";
 
-//Set the margins
-marginPrefs.properties = {
-  top: topMargin,     bottom: bottomMargin,
-  left: leftMargin,   right: rightMargin
-};
+  //Set the margins
+  margins.properties = {
+    top: halfInch,
+    bottom: halfInch,
+    left: halfInch,
+    right: halfInch
+  }
+  return doc;
+}
